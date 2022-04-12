3dc104472341d6f16a6c4bf0e507df7336130986
Milica <pavlovicm995@yahoo.com>
2022-04-12 21:15:28 +0200

    Task 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import TriviaApp from './pages/TriviaApp';
 
 function App() {
+  const [trivia, setTrivia] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav className="navbar-brand">
+          <ul>
+            <li>
+              <Link to='/'>Trivia</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route to='/'>
+            <TriviaApp/>
+          </Route>
+        </Switch>
+      <button onClick={() => setTrivia(true)}>Trivia</button>
+      <button onClick={() => setTrivia(false)}>Chuck</button>
+      {trivia ? <p>Trivia</p> : <p>Chuck</p>}
+      </Router>
     </div>
   );
 }
---
+++ src/pages/TriviaApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TriviaApp = () => {
+  return (
+    <div>TriviaApp</div>
+  )
+}
+
+export default TriviaApp
