8296d235dd0568c6aa7ef905d535ca32fd86397e
Milica <pavlovicm995@yahoo.com>
2022-04-12 21:25:17 +0200

    Task 2

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga/";
+import sagas from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: {},
+  middleware: [...getDefaultMiddleware(), sagaMiddleware],
+});
+
+for (let saga in sagas) {
+  sagaMiddleware.run(sagas[saga]);
+}
+
+export default store;
